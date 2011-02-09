dc639b89a78f3e98084b549011a492945acaa815
Jason Gross <jgross@mit.edu>
2011-02-09 15:35:58 -0500

    Added notes up to 2011-02-09.

---
+++ compile-all.bat
@@ -0,0 +1,6 @@
+pushd Notes by Day
+(rm "18.721 Lecture Notes 04 - 2011-02-11.pdf") & (pdflatex "18.721 Lecture Notes 04 - 2011-02-11.tex") & (rm "18.721 Lecture Notes 04 - 2011-02-11.pdf") & (pdflatex "18.721 Lecture Notes 04 - 2011-02-11.tex")
+(rm "18.721 Lecture Notes 03 - 2011-02-09.pdf") & (pdflatex "18.721 Lecture Notes 03 - 2011-02-09.tex") & (rm "18.721 Lecture Notes 03 - 2011-02-09.pdf") & (pdflatex "18.721 Lecture Notes 03 - 2011-02-09.tex")
+(rm "18.721 Lecture Notes 02 - 2011-02-07.pdf") & (pdflatex "18.721 Lecture Notes 02 - 2011-02-07.tex") & (rm "18.721 Lecture Notes 02 - 2011-02-07.pdf") & (pdflatex "18.721 Lecture Notes 02 - 2011-02-07.tex")
+(rm "18.721 Lecture Notes 01 - 2011-02-04.pdf") & (pdflatex "18.721 Lecture Notes 01 - 2011-02-04.tex") & (rm "18.721 Lecture Notes 01 - 2011-02-04.pdf") & (pdflatex "18.721 Lecture Notes 01 - 2011-02-04.tex")
+popd
